c798784c35486891dde7779999ad4f9eabf36717
sheillanjoroge <sheillan.njoroge@gmail.com>
2020-11-15 00:27:49 +0300

    add service to fetch repos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { GithubReposService } from './github-repos.service';
 
 @NgModule({
   declarations: [
