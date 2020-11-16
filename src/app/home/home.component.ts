25014dd2a3457fce47cbfebcb383e14dfc839933
sheillanjoroge <sheillan.njoroge@gmail.com>
2020-11-16 07:15:50 +0300

    add full landing page descriptions and some icons

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { GithubService } from '../github/github.service';
+import { Router } from '@angular/router';
+
+import { User } from '../user'
 
 @Component({
   selector: 'app-home',
@@ -6,10 +10,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  user;
 
-  constructor() { }
+  constructor(public githubService: GithubService,private router: Router) { }
 
   ngOnInit(): void {
+
+    console.log(this.githubService.getSearchedUserTerm())
+    if(this.githubService.getSearchedUserTerm() == undefined){
+      this.getDefaultUser()
+    }else{
+      this.getSearchedUser() 
+    }  
   }
 
+  async getDefaultUser(){
+    try{
+      this.user = await this.githubService.getUser('sheillanjoroge')
+      this.githubService.setRepo(this.user.repos_url)
+      console.log(this.user)
+    }catch(err){
+      this.user = err
+    }
+  }
+  
+  async getSearchedUser(){
+    try{
+      this.user = await this.githubService.getSearchedUser(this.githubService.getSearchedUserTerm())
+      console.log(this.user)
+    }catch(err){
+      this.user = err
+      console.log(this.user)
+    }
+  }
+
+  navigateHome(){
+    this.router.navigate(['/home'])
+  }
+
+  navigateRepos(){
+    this.githubService.setRepo(this.user.repos_url)
+    this.router.navigate(['/repositories'])
+  }
 }
